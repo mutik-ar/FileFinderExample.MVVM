3db7df6f8f9ef62f48d84aa52e3d7f6a7828efb3
Андрей Мутик <mutik.ar@gmail.com>
2024-05-26 15:32:14 +0300

    запуск программы через сборщик  реализаций View, Model и ViewModel

---
+++ Application/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FileFinderExample
+{
+    public partial class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Model.Model model = new();
+            ViewModel.ViewModel viewModel = new(model);
+            View.MainWindow mainWindow = new(viewModel);
+            mainWindow.ShowDialog();
+        }
+    }
+
+}

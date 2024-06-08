8a79854e0175184d2270ef8392f1586740bdb03e
Андрей Мутик <mutik.ar@gmail.com>
2024-06-08 19:46:13 +0300

    рефакторинг

--- IViewModel/vmComboBox.cs
+++ IViewModel/vmComboBox.cs
@@ -1,19 +1,13 @@
-﻿using Shared;
-using System.Collections.ObjectModel;
-
-
+﻿using System.Collections.ObjectModel;
 
 namespace Interfaces.ViewModel
 {
-    public class vmComboBox<T> : VisualEntity
+    public class vmComboBox<T> : ListEntity<T>
     {
 
 
         #region Fields
 
-        private ObservableCollection<T> _list;
-        private T _selectedItem ;
-        private int _selectedIndex = -1;
 
         #endregion
 
@@ -21,46 +15,20 @@ namespace Interfaces.ViewModel
 
         public vmComboBox()
         {
-            _list = new();
         }
 
-        public vmComboBox(ObservableCollection<T> list)
+        public vmComboBox(ObservableCollection<T> list) : base(list)
         {
-            _list = list;
+        }
+
+        public vmComboBox(ObservableCollection<T> list, string text) : base(list, text)
+        {
         }
 
 
         #endregion
 
         #region Properties
-        public ObservableCollection<T> List
-        {
-            get
-            {
-                return _list;
-            }
-
-        }
-
-        public T SelectedItem
-        {
-            get { return _selectedItem; }
-            set
-            {
-                _selectedItem = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public int SelectedIndex
-        {
-            get { return _selectedIndex; }
-            set
-            {
-                _selectedIndex = value;
-                OnPropertyChanged();
-            }
-        }
 
         #endregion
     }

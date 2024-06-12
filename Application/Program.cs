7feb1d037dc24735d875315dcf05816916c3e9ff
Андрей Мутик <mutik.ar@gmail.com>
2024-06-12 17:19:38 +0300

    внедрен MEF (внешнее подключение библиотек Model и ViewModel)

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,14 +1,37 @@
-﻿namespace FileFinderExample
+﻿using Interfaces.Model;
+using Interfaces.ViewModel;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Registration;
+
+namespace FileFinderExample
 {
     public partial class Program
     {
+        public IModel? Model { get; set; }
+        public IViewModel ViewModel { get; set; }
+
         [STAThread]
         public static void Main()
         {
-            Model.Model model = new();
-            ViewModel.ViewModel viewModel = new(model);
-            View.MainWindow mainWindow = new(viewModel);
+            Program program = new Program();
+            program.Composite();
+        }
+
+        public void Composite()
+        {
+            var conventions = new RegistrationBuilder();
+            conventions.ForTypesDerivedFrom<IModel>().Export<IModel>();
+            conventions.ForType<Program>().ImportProperty<IModel>(p => p.Model);
+            conventions.ForTypesDerivedFrom<IViewModel>().Export<IViewModel>();
+            conventions.ForType<Program>().ImportProperty<IViewModel>(p => p.ViewModel);
+            var catalog = new DirectoryCatalog(AppDomain.CurrentDomain.BaseDirectory, conventions);
+            CompositionService service = catalog.CreateCompositionService();
+            service.SatisfyImportsOnce(this, conventions);
+
+            View.MainWindow mainWindow = new(ViewModel);
             mainWindow.ShowDialog();
+
         }
     }
 
